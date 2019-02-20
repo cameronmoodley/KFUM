64e5433fc524d8f30bac2ee50de24a356f083271
hortasha <eidemartin_303@hotmail.com>
2019-02-20 12:01:08 +0100

    created all dumb component files and got api working

--- src/pages/Course-Page/Course-Page.jsx
+++ src/pages/Course-Page/Course-Page.jsx
@@ -5,65 +5,78 @@ class CoursePage extends Component {
 
   constructor(props){
       super(props)
-       this.state = {
-           posts: [],
-           moduleName: "",
-           slides: []
-       }
-       this.getData = this.getData.bind(this);
-       this.getSlides = this.getSlides.bind(this);
+      this.state = {
+        courses: [],
+        modules: [],
+        slides: [],
+        activeModule: "",
+        activeSlide: "",
+        activeCourse: ""
+      }
+      this.getData = this.getData.bind(this);
   }
-
 
   componentDidMount() {
-    this.getData();
+    this.getData("courses");
   }
 
-  getData(){
-    fetch('https://kfum-kfuk.herokuapp.com/posts')
+  getData(type){
+    fetch('https://kfuk-kfum.herokuapp.com/' + type)
     .then( (response) => {
         return response.json();
     })
     .then((result) => {
         console.log('result',result);
         this.setState({
-            posts: result
+            courses: result
         })
+        console.log(this.state.courses[0].name)
     })
   }
 
- /*
-  getSlides() {
-    fetch('https://kfum-kfuk.herokuapp.com/ ' + this.moduleName + ' /slides')
-    .then( (response) => {
-        return response.json();
-    })
-    .then((result) => {
-        console.log('slides',result);
-        this.setState({
-          slides: result
-        })
-    })
+  addData(type) {
+
   }
-  slideContent() {
-    const withoutRows = this.state.slides.map(i => <div className="col-md-6"><ModuleSlide>{i}</ModuleSlide></div>);
-  }*/
+
+  updateData(type, id, content) {
+
+  }
+
+  deleteData(type, id) {
+
+  }
 
  //You should only make api calls in these pages   
   render() {
     return (
       <div>
         <div className="[ courseDetail ][ row ]">
-          <div className="[ col-12 ]">
-            <CourseDetail></CourseDetail>
+          <div className="[ col-md-6 ]">
+            {/* Text components with course data from api will be added here */}
+          </div>
+          <div className="[ col-md-6 ]">
+            {/* Text components with course data from api will be added here */}
           </div>
         </div>
 
         <div className="[ modules ][ row ]">
+          {/* Module components with data from api will be added here */}
         </div>
 
-        <div className="[ slides ]">
-          {/*slideContent()*/}
+        <div className="[ module-info ][ row ]">
+          <div className="[ col-md-6 ]">
+            {/* Text components with module data from api will be added here */}          
+          </div>
+          <div className="[ col-md-6 ]">
+            {/* Text components with module data from api will be added here */}
+          </div>
+        </div>
+        
+        <div className="[ slides ][ row ]">
+          {/* Slide components with data from api will be added here*/}
+
+          {/* Trying to figure out how to map out all course names in API */}
+          {(this.state.courses.length !== 0) ? this.state.courses.map(i => <p>{i.name}</p>) : ""}
         </div>
       </div>
     );
