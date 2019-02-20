f214ad9a900fca55c978b4f3276dec1b9e9ef39a
Thomassen <noveng3ance@gmail.com>
2019-02-20 11:37:41 +0100

    added module slide
    added slide code with title, img src and methods recieved from parent

--- src/components/module-slide/module-slide.jsx
+++ src/components/module-slide/module-slide.jsx
@@ -7,3 +7,27 @@
  * 
  * You should also have a option to edit this information
  */
+
+import React from 'react';
+
+class Slide extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = ({
+            slide: {},
+            method: this.props.function
+        })
+    }
+
+    render(){
+        return(
+            <div className="Slide" onClick={ this.state.method }>
+                <h2> { this.state.id } </h2>
+                <p> { this.props.children } </p>
+                <img src={ this.state.src } alt={ this.state.title } />
+            </div>
+        )
+    }
+}
+
+export default Slide;
